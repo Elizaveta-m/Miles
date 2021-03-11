5dadd868409ed4fe509fc07b62b8acf8c77678eb
Elizaveta Morozova <rusquee@gmail.com>
2021-03-12 00:59:32 +0300

    Deleted registered

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        boolean registered = true;
-        if (registered) {
-            int ticketPrice = 20000;
-            int priceStep = 20;
-            int bonusScore = 1;
-            int totalBonus = ticketPrice / priceStep * bonusScore;
-            System.out.println(totalBonus);
-        }
+        int ticketPrice = 20000;
+        int priceStep = 20;
+        int bonusScore = 1;
+
+        int totalBonus = ticketPrice / priceStep * bonusScore;
+        System.out.println(totalBonus);
     }
 }
